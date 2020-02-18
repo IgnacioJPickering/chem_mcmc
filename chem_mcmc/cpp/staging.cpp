811b545dba45d1738da053db90bcb6bff1693883
ipickering <ign.pickering@gmail.com>
2020-02-17 23:34:08 -0500

    Improve the cpp Bounds class

--- chem_mcmc/cpp/staging.cpp
+++ chem_mcmc/cpp/staging.cpp
@@ -13,8 +13,8 @@ class Bounds {
   std::vector<double> upper_;
   std::vector<double> sizes_;
   int dimension_;
-  Bounds(const std::string &kind, const std::vector<double> &lower
-         ,const std::vector<double> &upper) {
+  Bounds(const std::string &kind="n", const std::vector<double> &lower={0.}
+         ,const std::vector<double> &upper={10.}) {
     lower_ = lower;
     upper_ = upper;
     kind_ = kind;
@@ -22,6 +22,16 @@ class Bounds {
     for (int j=0; j != int(lower_.size()); j+=1){
       sizes_.push_back(upper_[j] - lower_[j]);
     }
+  }
+  //Factory method
+  static Bounds square(double lower=0.0, double upper=0.0, std::string kind="n", int dimension=1){
+    std::vector<double> lower_vec;
+    std::vector<double> upper_vec;
+    for (int j=0;j != dimension; j++){
+      lower_vec.push_back(lower);
+      upper_vec.push_back(upper);
+    }
+    return Bounds(kind, lower_vec, upper_vec);
   }
 
   bool areInBounds(const std::vector<double> &coordinates) {
@@ -63,11 +73,26 @@ class Bounds {
 
 // This is binding code for pybind11, usually binding code and
 // implementation are in different files
+// Basically the ONLY thing I have to write is the PYBIND11 binding code, 
+// The rest of the code is normal Cpp code, 100% normal
+// Keep in mind that the pybind namespace defines some easy utilities
+// for interfacing with C++ such as py::none and others
+// def_readonly and def_readwrite are used for attributes
+// def is used for functions and def_static for static methods (classmethods
+// in python)
+// Names can be changed for usage in python, which can be very useful 
+// to keep python conventions!
 PYBIND11_MODULE(staging, m) {
   m.doc() = "Bounds class implemented in cpp";  // optional module docstring
   py::class_<Bounds>(m, "Bounds")
       .def(py::init< const std::string &, const std::vector<double> &, const std::vector<double> &>())
-      .def("areInBounds", &Bounds::areInBounds, py::arg("coordinates"))
-      .def("getWrappedDistance", &Bounds::getWrappedDistance, py::arg("coordinates1"), py::arg("coordinates2"))
-      .def("wrapCoordinates", &Bounds::wrapCoordinates, py::arg("difference"));
+      .def_readonly("kind", &Bounds::kind_)
+      .def_readonly("lower", &Bounds::lower_)
+      .def_readonly("upper", &Bounds::upper_)
+      .def_readonly("sizes", &Bounds::sizes_)
+      .def_readonly("dimension", &Bounds::dimension_)
+      .def("are_in_bounds", &Bounds::areInBounds, py::arg("coordinates"))
+      .def("get_wrapped_distance", &Bounds::getWrappedDistance, py::arg("coordinates1"), py::arg("coordinates2"))
+      .def("wrap_coordinates", &Bounds::wrapCoordinates, py::arg("difference"))
+      .def_static("square", &Bounds::square, py::arg("lower")=0.0, py::arg("upper")=0.0, py::arg("kind")="n", py::arg("dimension")=1);
 }
